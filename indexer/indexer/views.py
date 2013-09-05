f863a7fe7480af080d936764ec0daea9d09b9f7e
Reindexer <reindexer@gmail.com>
2013-09-05 13:22:42 +0800

    完善了的searchapi, 增加分组，分页，排序模式，增加详情接口

--- indexer/indexer/views.py
+++ indexer/indexer/views.py
@@ -5,20 +5,32 @@ from django.views.decorators.csrf import csrf_exempt
 import apis
 import json
 
+INDEX = 'steel_package'
+PAGE_SIZE = 20
+
 def reindex(request):
-	searcher = apis.Searcher('steel_package', settings.INDEX_SERVER)
-	result = searcher.reindex()
-	return HttpResponse(result)
+    searcher = apis.Searcher(INDEX, settings.INDEX_SERVER)
+    result = searcher.reindex()
+    return HttpResponse(result)
 
 @csrf_exempt
 def update(request):
-	searcher = apis.Searcher('steel_package', settings.INDEX_SERVER)
-	rows = json.loads(request.body)
-	result = searcher.update(rows)
-	print result
-	return HttpResponse(result)
+    searcher = apis.Searcher(INDEX, settings.INDEX_SERVER)
+    rows = json.loads(request.body)
+    result = searcher.update(rows)
+    return HttpResponse(result)
 
-def query(request, keyword):
-	searcher = apis.Searcher('steel_package', settings.INDEX_SERVER)
-	result = searcher.query(keyword)
-	return HttpResponse(json.dumps(result), 'application/json')
+def query(request):
+    keyword = request.GET.get('query', '')
+    page = int(request.GET.get('page', '0'))
+    sort = int(request.GET.get('sort', '0'))
+    searcher = apis.Searcher(INDEX, settings.INDEX_SERVER)
+    result = searcher.query(keyword, sort, page*PAGE_SIZE, PAGE_SIZE)
+    return HttpResponse(json.dumps(result), 'application/json')
+
+def detail(request, group_hash):
+    keyword = request.GET.get('query', '')
+    sort = int(request.GET.get('sort', '0'))
+    searcher = apis.Searcher(INDEX, settings.INDEX_SERVER)
+    result = searcher.detail(group_hash, keyword, sort)
+    return HttpResponse(json.dumps(result), 'application/json')
--- search/search/static/js/app.js
+++ search/search/static/js/app.js
@@ -9,12 +9,19 @@ angular.module('818717', ['ui.bootstrap']).
 var TabsCtrl = function ($scope, $http) {
   var websites = [{ title: 'python'}];
 
-  $http.get('/search/query/上海/').success(function(data){
+  $http.
+    get('/search/query/', {
+      params: { 
+        query: 'spcc',
+        sort: 0,
+        page: 0,
+      }
+    }).
+    success(function(data){
       $scope.groups = [];
       angular.forEach(data.result, function(row) {
-        var resource = angular.fromJson(row[2]);
-        resource.id = row[0];
-        resource.hit_count = row[1];
+        var resource = angular.fromJson(row[0]);
+        resource.sort_by = row[1];
         $scope.groups.push(resource);
       });
   });
