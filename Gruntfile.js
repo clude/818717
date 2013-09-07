68495a3040178bbb7bde74dde8f30e6af35163cf
Reindexer <reindexer@gmail.com>
2013-09-08 01:04:40 +0800

    增加了conf配置

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,10 +18,12 @@ module.exports = function ( grunt ) {
   var userConfig = {
     target_dir: path.join(cwd, 'build'),
     log_dir: path.join(cwd, 'log'),
-    crawler_dir: path.join(cwd, 'crawler'),
+    crawler_dir: path.join(cwd, 'crawler'),    
     product_name: '818717',
     index_server: '127.0.0.1',
   };
+
+  userConfig['site_dir'] = path.join(userConfig.target_dir, 'site');
 
   var taskConfig = {
     meta: {
@@ -46,7 +48,7 @@ module.exports = function ( grunt ) {
         files: [
           { 
             src: [ '**/*.py', '**/index.html' ],
-            dest: '<%= target_dir %>',
+            dest: '<%= site_dir %>',
             cwd: 'search',
             expand: true
           }
@@ -56,12 +58,12 @@ module.exports = function ( grunt ) {
         files: [
           { 
             src: [ '**' ],
-            dest: '<%= target_dir %>/search/static/',
+            dest: '<%= site_dir %>/search/static/',
             cwd: 'search/search/static',
             expand: true
           }
         ]
-      }
+      },
     },
 
     html2js: {
@@ -70,7 +72,7 @@ module.exports = function ( grunt ) {
           base: 'search/search'
         },
         src: [ 'search/search/static/partials/*.html' ],
-        dest: '<%= target_dir %>/search/static/js/template_cache.js'
+        dest: '<%= site_dir %>/search/static/js/template_cache.js'
       }
     },
 
@@ -79,8 +81,8 @@ module.exports = function ( grunt ) {
         files: [
           {
             src: [ '**/*.js' ],
-            cwd: '<%= target_dir %>/search/static/js',
-            dest: '<%= target_dir %>/search/static/js',
+            cwd: '<%= site_dir %>/search/static/js',
+            dest: '<%= site_dir %>/search/static/js',
             expand: true
           }
         ]        
@@ -93,10 +95,10 @@ module.exports = function ( grunt ) {
           banner: '<%= meta.banner %>'
         },
         src: [ 
-          '<%= target_dir %>/search/static/js/**/*.js', 
+          '<%= site_dir %>/search/static/js/**/*.js', 
           '<%= html2js.partials.dest %>', 
         ],
-        dest: '<%= target_dir %>/search/static/js/<%= product_name %>.js'
+        dest: '<%= site_dir %>/search/static/js/<%= product_name %>.js'
       }
     },
 
@@ -106,15 +108,15 @@ module.exports = function ( grunt ) {
           banner: '<%= meta.banner %>',
         },
         files: {
-          '<%= target_dir %>/search/static/js/<%= product_name %>.min.js': '<%= concat.compile.dest %>'
+          '<%= site_dir %>/search/static/js/<%= product_name %>.min.js': '<%= concat.compile.dest %>'
         }        
       }
     },
 
     recess: {
       compile: {
-        src: [ '<%= target_dir %>/search/static/css/main.less' ],
-        dest: '<%= target_dir %>/search/static/css/<%= product_name %>.min.css',
+        src: [ '<%= site_dir %>/search/static/css/main.less' ],
+        dest: '<%= site_dir %>/search/static/css/<%= product_name %>.min.css',
         options: {
           compile: true,
           compress: true,
@@ -124,27 +126,57 @@ module.exports = function ( grunt ) {
 
     exec: {
       testbuild: {
-        command: 'python <%= target_dir %>/manage.py runserver 8001'
+        command: 'python <%= site_dir %>/manage.py runserver 8001'
       },
       pip: {
         command: 'pip install -r search/requirements.txt'
       },
       npm: {
         command: 'npm install',
-        cwd: 'crawler'
+        cwd: '<%= crawler_dir %>'
       },
       localrun: {
         command: 'python search/manage.py runserver 8001'
-      },
-      reindex: {
-        command: 'python search/manage.py reindex'
       },
       install_searchapi: {
         command: 'python setup.py install',
         cwd: 'indexer'
       },
+
+      start_nginx: {
+        command: 'nginx -c <%= target_dir %>/conf/nginx.conf'
+      },
+      stop_nginx: {
+        command: 'nginx -s stop'
+      },
+
+      start_sphinx: {
+        command: 'searchd -c <%= target_dir %>/conf/sphinx.conf'
+      },
+      clean_sphinx: {
+        command: 'rm <%= log_dir =>/sphinx/steel_package.*'
+      },
+      stop_sphinx: {
+        command: 'searchd -c <%= target_dir %>/conf/sphinx.conf --stop'
+      },
       test_sphinx: {
         command: "mysql -h <%= index_server %> -P 9217 -e 'show tables; desc steel_package; select count(*) from steel_package;'"
+      },
+      reindex: {
+        command: 'python search/manage.py reindex'
+      },
+
+      start_supervisor: {
+        command: 'supervisord -c <%= target_dir %>/conf/supervisord.conf;'
+      },
+      reload_supervisor: {
+        command: 'supervisorctl reload'
+      },
+      restart_gunicorn: {
+        command: 'supervisorctl restart site'
+      },
+      restart_crawler: {
+        command: 'supervisorctl restart server; supervisorctl restart node;'
       },
     }
   };
@@ -153,12 +185,12 @@ module.exports = function ( grunt ) {
 
   grunt.registerTask('index', function() {
     var
-      index_html = grunt.config('target_dir')+'/search/templates/index.html',
-      js_min = grunt.config('target_dir')+'/search/static/js/'+grunt.config('product_name')+'.min.',
+      index_html = grunt.config('site_dir')+'/search/templates/index.html',
+      js_min = grunt.config('site_dir')+'/search/static/js/'+grunt.config('product_name')+'.min.',
       js_min_sha1 = sha1(grunt.file.read(js_min+'js')),
-      js_template = grunt.config('target_dir')+'/search/static/js/template_cache.',
+      js_template = grunt.config('site_dir')+'/search/static/js/template_cache.',
       js_template_sha1 = sha1(grunt.file.read(js_template+'js')),
-      css_min = grunt.config('target_dir')+'/search/static/css/'+grunt.config('product_name')+'.min.',
+      css_min = grunt.config('site_dir')+'/search/static/css/'+grunt.config('product_name')+'.min.',
       css_min_sha1 = sha1(grunt.file.read(css_min+'css'));
 
     grunt.file.copy(js_min+'js', js_min+js_min_sha1+'.js');
@@ -181,21 +213,28 @@ module.exports = function ( grunt ) {
     console.log('index.html processed.');
   });
 
+  grunt.registerTask('conf', function() {
+    ['conf/nginx.conf', 'conf/sphinx.conf', 'conf/supervisord.conf'].forEach(function(conf){
+      grunt.file.copy(path.join(cwd, conf), path.join(grunt.config('target_dir'), conf), {
+        process: function (contents, path)  {
+          return grunt.template.process(contents, userConfig);
+        }
+      });
+    });
+  });
+
   grunt.registerTask('default', ['compile']);
 
-  grunt.registerTask('compile', ['clean', 'copy', 'ngmin', 'concat', 'uglify', 'html2js', 'recess', 'index']);
+  grunt.registerTask('compile', ['clean', 'copy', 'ngmin', 'concat', 'uglify', 'html2js', 'recess', 'index', 'conf']);
   grunt.registerTask('testbuild', ['compile', 'exec:pip', 'exec:testbuild']);
   grunt.registerTask('run', ['exec:pip', 'exec:localrun']);
   grunt.registerTask('upgrade', ['exec:pip', 'exec:npm']);
   grunt.registerTask('install_searchapi', ['exec:install_searchapi']);
-  grunt.registerTask('test_sphinx', ['exec:test_sphinx']);
-  grunt.registerTask('reindex', ['exec:reindex']);
 
-  grunt.registerTask('stop_nginx', []);
-  grunt.registerTask('start_nginx', []);
-  grunt.registerTask('stop_nginx', []);
-  grunt.registerTask('stop_nginx', []);
-  grunt.registerTask('stop_nginx', []);
-  grunt.registerTask('stop_nginx', []);
+  grunt.registerTask('config_sphinx', ['exec:stop_sphinx', 'exec:clean_sphinx', 'exec:start_sphinx']);
+  grunt.registerTask('restart', ['exec:stop_nginx', 'compile', 'exec:pip', 'exec:install_searchapi', 'exec:restart_gunicorn', 'exec:start_nginx']);
+  grunt.registerTask('restart_sphinx', ['config_sphinx', 'exec:reindex']);
+  grunt.registerTask('restart_crawler', ['exec:npm', 'exec:restart_crawler']);
+
 };
 
